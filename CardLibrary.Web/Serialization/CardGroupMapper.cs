a4570e639d3927702a16afdceff5741d08fd8b72
nupurc <nupurc.chatterjee@gmail.com>
2020-09-13 20:31:22 +0530

    ViewModel and Mapper addition

---
+++ CardLibrary.Web/Serialization/CardGroupMapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardLibrary.Web.Serialization
+{
+    public class CardGroupMapper
+    {
+    }
+}
