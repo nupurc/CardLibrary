3abde1abca5ce85cc7800b5f8504eb1a876e660f
nupurc <nupurc.chatterjee@gmail.com>
2020-10-01 14:26:01 +0530

    new

--- CardLibrary.Web/Startup.cs
+++ CardLibrary.Web/Startup.cs
@@ -1,22 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CardLibrary.Data;
 using CardLibrary.Services.Card;
 using CardLibrary.Services.Group;
 using CardLibrary.Services.User;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
 
 namespace CardLibrary.Web
 {
@@ -51,7 +45,12 @@ namespace CardLibrary.Web
             services.AddTransient<ICardGroupService, CardGroupService>();
             services.AddTransient<IUserRoleService, UserRoleService>();
             services.AddTransient<IUserGroupService, UserGroupService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
+    
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -79,7 +78,11 @@ namespace CardLibrary.Web
             {
                 endpoints.MapControllers();
             });
-            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
